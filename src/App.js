c557dff09ac89df0b8788e396bff4c6e59bc6c7c
moonibo <115149028+moonibo@users.noreply.github.com>
2022-10-19 14:15:57 +0200

    creacion de menu y estilos
    creacion esqueleto, menu y barra navegacion + modales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { ReactDimmer } from 'react-dimmer';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from "react";
+import { Modal } from "./components/Modal";
+import { Menu } from "./components/Menu";
+import Map from './images/mapa.jpeg';
+import './stylesheets/stylesMenu.css';
+import './stylesheets/stylesModal.css';
 
 function App() {
+
+  const [isModalOpen, setModal] = useState(false);
+  const [isMenuOpen, setMenu] = useState(false);
+
+  const handleClick = () => {
+    setModal((prevState) => !prevState);
+  };
+
+  const handleMenu = () => {
+    setMenu((prevState) => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <div className='header'>
+          <div className='nav'>
+            <GiHamburgerMenu className='menu' onClick={handleMenu} />
+            <h1>EChargeMap</h1>
+            <div className='buttons'>
+              <button className='home buttons-nav' onClick={handleClick}>Inicio </button>
+              <button className='login buttons-nav' onClick={handleClick}>Entrar </button>
+              <button className='language buttons-nav' onClick={handleClick}>Idioma </button>
+              <button className='share buttons-nav' onClick={handleClick}>Compartir </button>
+            </div>
+          </div>
+        </div>
+        <div className='body'>
+          <div className='container-main'>
+            <div className='container-map'>
+              <img className='map-photo' src={Map} alt='mapa' />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {isModalOpen && <Modal closeModal={setModal} />}
+      <Menu isMenuOpen={isMenuOpen} />
+
+      <ReactDimmer
+        isOpen={isModalOpen}
+        exitDimmer={setModal}
+        zIndex={100}
+        blur={1.5}
+      />
+      <ReactDimmer
+        isOpen={isMenuOpen}
+        exitDimmer={setMenu}
+        zIndex={100}
+        blur={1.5}
+      />
+    </>
   );
 }
 
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+/*interface IMenuProps {
+    isMenuOpen;
+}*/
+
+export const Menu = ({ isMenuOpen }) => {
+    return (
+      <div className={`app-menu ${isMenuOpen ? 'menu-open' : ''}`} >
+        <h2>Example</h2>
+      </div>
+    );
+}
